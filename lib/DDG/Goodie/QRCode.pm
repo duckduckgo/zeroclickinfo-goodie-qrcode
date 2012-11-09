c82e5048dd96678b3dafec9b27cb9944c82fb016
Michael Smith <crazedpsyc@duckduckgo.com>
2012-11-09 08:23:34 -0700

    Add metadata to QRCode goodie

--- lib/DDG/Goodie/QRCode.pm
+++ lib/DDG/Goodie/QRCode.pm
@@ -9,6 +9,14 @@ triggers start => 'qrcode', 'qr code', 'qr';
 zci is_cached => 1;
 zci answer_type => "qrcode";
 
+attribution github => ['https://github.com/mstratman', 'mstratman'];
+primary_example_queries 'qrcode http://ddg.gg';
+description 'generate a QR code from your query';
+name 'QRCode';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodie-qrcode/blob/master/lib/DDG/Goodie/QRCode.pm';
+category 'computing_tools';
+topics 'cryptography';
+
 handle remainder => sub {
     my $html = HTML::Barcode::QRCode->new(text => $_)->render;
     $html = '<div style="float:left;margin-right:10px;">'.$html.'</div> A QR code that means \''.encode_entities($_).'\'. <div class="clear"></div>';
