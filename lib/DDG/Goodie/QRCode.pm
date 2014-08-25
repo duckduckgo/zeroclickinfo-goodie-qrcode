e550aa10520764e5f8269958038b08308e6262b2
Usman Raza <usman.r123@gmail.com>
2014-08-25 17:43:15 +0500

    revamped Qrcode

--- lib/DDG/Goodie/QRCode.pm
+++ lib/DDG/Goodie/QRCode.pm
@@ -29,16 +29,30 @@ my $qrcode = Imager::QRCode->new(
     darkcolor     => Imager::Color->new(0, 0, 0),
 );
 
-die "No GIF support available; install giflib (libgif-dev in Debian)" unless $Imager::formats{gif};
+die "No GIF support available; install giflib (libgif-dev in Debian), and then install Imager (to be built with GIF support)." unless $Imager::formats{gif};
+
+sub html_output {
+    my ($data, $string) = @_;
+    return '<div style="float:left;margin-right:10px;">'
+            .'<img src="data:image/gif;base64,'.$data.'" alt="A QR Code" /></div>'
+            .'A QR code that means \''.encode_entities($_).'\'.'
+            .'<div class="clear"></div>';
+}
 
 handle remainder => sub {
-    my $image = $qrcode->plot($_);
-    my $raw;
-    $image->write(data => \$raw, type => 'gif');
-    my $data = encode_base64($raw);
-    $data =~ s/\n//g;
-    my $html = '<div style="float:left;margin-right:10px;"><img src="data:image/gif;base64,'.$data.'" alt="A QR Code" /></div> A QR code that means \''.encode_entities($_).'\'. <div class="clear"></div>';
-    return '', html => $html;
+    return if /^(reader|generator|scanner|maker|creator|builder)$/;
+    s/^(of|for) (.*)$/$2/;    # Remove of or for in queries like 'qr of http://ddg.gg'
+    s/^"(.*)"$/$1/;    # Remove quotes from queries like 'qr "reader"' to ignore skipped words
+    if (/^\s*(.*)\s*$/) {
+        return unless $1;
+        my $str = $1;
+        my $image = $qrcode->plot($str);
+        my $raw;
+        $image->write(data => \$raw, type => 'gif');
+        my $data = encode_base64($raw);
+        $data =~ s/\n//g;
+        return '', html => html_output ($data, $str);
+    }
 };
 
 1;
